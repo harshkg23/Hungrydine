f6583792692eb07d25cc6ad160a43a28d990b837
Harsh Kumar Gupta <harshkumargupta2023@gmail.com>
2024-12-26 15:02:37 +0530

    Update StoreContext to use production URL for API

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -8,7 +8,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = ({ children }) => {
 
   const [cartItems,setCartItems] = useState({})
-  const url = "http://localhost:4000";
+  const url = "https://hungrydine.onrender.com";
   const [token,setToken] = useState("");
   const [food_list,setFoodList] = useState([]);
 
